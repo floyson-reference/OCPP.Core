5b17c90beaa0d50efb5f706a26d851bed726fb06
Frederic Loyson <f.loyson@telenet.be>
2025-12-20 21:09:38 +0100

    Try to write persistkeys to d drive

--- OCPP.Core.Management/Startup.cs
+++ OCPP.Core.Management/Startup.cs
@@ -18,11 +18,13 @@
  */
 
 using System;
+using System.IO;
 using Hangfire;
 using Hangfire.Dashboard;
 using Hangfire.Dashboard.BasicAuthorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
@@ -51,6 +53,9 @@ namespace OCPP.Core.Management
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(@"D:\home\site\dataprotection-keys"));
+
             services.AddOCPPDbContext(Configuration);
 
             services.AddControllersWithViews();
